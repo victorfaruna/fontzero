5b77b5b4e62af22db7087da65b4ecff3f64416f0
victorfaruna <farunavictor043@gmail.com>
2024-09-04 11:08:48 +0100

    uwqdu

--- static/script/embedicons.js
+++ static/script/embedicons.js
@@ -1,5 +1,16 @@
-const linkElement = document.createElement('link');
-linkElement.setAttribute('rel', 'stylesheet');
-linkElement.setAttribute('href', 'demons');
+const urls = [
+	'https://site-assets.fontawesome.com/releases/v6.6.0/css/all.css',
+	'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-duotone-solid.css',
+	'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-thin.css',
+	'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-solid.css',
+	'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-regular.css',
+	'https://site-assets.fontawesome.com/releases/v6.6.0/css/sharp-light.css'
+];
 
-document.head.appendChild(linkElement);
+urls.forEach((url) => {
+	const linkElement = document.createElement('link');
+	linkElement.setAttribute('rel', 'stylesheet');
+	linkElement.setAttribute('href', url);
+
+	document.head.appendChild(linkElement);
+});
