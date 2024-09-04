1a88f12b4fc88c5e22df89de0d763aa590367979
victorfaruna <farunavictor043@gmail.com>
2024-09-04 11:04:02 +0100

    jbwdi

--- static/script/embedicons.js
+++ static/script/embedicons.js
@@ -1,3 +1,5 @@
 const linkElement = document.createElement('link');
 linkElement.setAttribute('rel', 'stylesheet');
 linkElement.setAttribute('href', 'demons');
+
+document.head.appendChild(linkElement);
