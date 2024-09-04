495d6de7db07ae4da893fbf68676e5839c6422ad
victorfaruna <farunavictor043@gmail.com>
2024-09-04 11:35:08 +0100

    yes man

--- src/components/common/Header.svelte
+++ src/components/common/Header.svelte
@@ -1,7 +1,10 @@
 <script>
-	import { goto } from '$app/navigation';
+	let isSideNavShown = false;
 </script>
 
+<div
+	class={`sidenav fixed z-[200] hidden h-screen w-[70vw] backdrop-blur-[5px] transition-all duration-[0.3s] sm:block ${!isSideNavShown ? 'translate-x-[-100%]' : 'translate-x-0'} bg-black/50`}
+></div>
 <header class="flex h-[100px] w-full items-center justify-center">
 	<div
 		class="inner flex h-auto w-[95%] items-center justify-between rounded-3xl border border-color-2/10 bg-color-2/10 px-3 py-2 shadow-lg backdrop-blur-[10px]"
@@ -12,9 +15,9 @@
 		</div>
 		<nav class="flex gap-10 sm:hidden">
 			<li><a href="/#">Home</a></li>
-			<li><a href="/#">Features</a></li>
-			<li><a href="/#">Updates</a></li>
-			<li><a href="/#">Pricing</a></li>
+			<li><a href="/#">Support</a></li>
+			<li><a href="/#">Github</a></li>
+			<li><a href="/#">Twitter</a></li>
 		</nav>
 		<a
 			href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
@@ -22,6 +25,9 @@
 			class="btn flex h-[30px] w-[110px] items-center justify-center gap-2 rounded-2xl bg-color-3 text-color-5 sm:hidden"
 			>Login <i class="fa-light fa-arrow-right"></i></a
 		>
-		<div class="hidden text-[20px] sm:block"><i class="fa-regular fa-bars"></i></div>
+		<button on:click={() => (isSideNavShown = !isSideNavShown)} class="hidden text-[20px] sm:block">
+			<i class:fa-bars={!isSideNavShown} class:fa-xmark-large={isSideNavShown} class="fa-regular"
+			></i>
+		</button>
 	</div>
 </header>
